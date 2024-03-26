acb68e6dc0bc4a6316179f07734e42c8fd366031
MrDanikYT-pixel <83244543+MrDanikYT-pixel@users.noreply.github.com>
2024-03-26 20:46:03 +0100

    1.0

---
+++ functions.py
@@ -0,0 +1,268 @@
+import os
+import aiosqlite
+import random
+import re
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+import hashlib
+import base64
+
+
+def generate_random_word(length) -> str:
+    pattern = r'^[0-9a-z.#]+$'
+    length = length
+    regex_pattern = re.compile(pattern)
+    vowels = 'aeiou'
+    consonants = 'bcdfghjklmnpqrstvwxyz'
+    word = ''
+
+    while not regex_pattern.match(word):
+        word = ''
+        vowel_count = 0
+        consonant_count = 0
+        last_vowel = ''
+
+        while len(word) < length:
+            rand = random.randint(1, 2)
+
+            if rand == 1:
+                if vowel_count >= 2:
+                    continue
+                else:
+                    vowel = random.choice([v for v in vowels if v != last_vowel])
+                    last_vowel = vowel
+                    vowel_count += 1
+                    consonant_count = 0
+                    word += vowel
+            elif rand == 2:
+                if consonant_count >= 1:
+                    continue
+                else:
+                    consonant_count += 1
+                    vowel_count = 0
+                    word += random.choice(consonants)
+
+        for _ in range(int(length/20)):
+            index = random.randint(2, length-6)
+            symbol = "."
+            word = word[:index] + symbol + word[index+1:]
+        index = length-5
+
+    return word
+
+
+def sha256(string: str) -> str:
+    return hashlib.sha256(string.encode()).hexdigest()
+
+
+def sha512(string: str) -> str:
+    return hashlib.sha512(string.encode()).hexdigest()
+
+
+def generate_passphrase(count, min_length, max_lenght) -> str:
+    list = [generate_random_word(random.randint(min_length, max_lenght)) for _ in range(count)]
+    return ' '.join(list)
+
+
+def generate_rsa_key(passphrase: str) -> tuple[bytes]:
+    passphrase = sha512(passphrase)
+    key = RSA.generate(2048)
+    encrypted_key = key.export_key(passphrase=passphrase, pkcs=8,
+                                   protection="scryptAndAES128-CBC")
+    public_key = key.publickey().export_key(passphrase=passphrase, pkcs=8,
+                                            protection="scryptAndAES128-CBC")
+    return encrypted_key, public_key
+
+
+def encrypt_message(message: str | bytes, public_key: bytes | str, passphrase: str) -> str:
+    if isinstance(message, str):
+        message = message.encode()
+    if isinstance(public_key, str):
+        public_key = public_key.encode()
+    passphrase = sha512(passphrase)
+    public_key = RSA.import_key(public_key, passphrase=passphrase)
+    cipher = PKCS1_OAEP.new(public_key)
+    encrypted_message = cipher.encrypt(message)
+    return base64.b64encode(encrypted_message).decode()
+
+
+def decrypt_message(encrypted_message: str | bytes, private_key: bytes | str, passphrase: str) -> str:
+    if isinstance(encrypted_message, str):
+        encrypted_message = encrypted_message.encode()
+    if isinstance(private_key, str):
+        private_key = private_key.encode()
+    passphrase = sha512(passphrase)
+    private_key = RSA.import_key(private_key, passphrase=passphrase)
+    cipher = PKCS1_OAEP.new(private_key)
+    decoded_message = base64.b64decode(encrypted_message)
+    decrypted_message = cipher.decrypt(decoded_message)
+    return decrypted_message.decode()
+
+
+def get_room_id(passphrase: str) -> int:
+    passphrase = sha512(passphrase)
+    return random.Random(passphrase).randint(0, 10**25)
+
+
+def room_db(id: int) -> str:
+    folder = './rooms/'
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    return f"{folder}{id}.db"
+
+
+class Room():
+    def __init__(self, id: int, passphrase: str, public_key: bytes, private_key: bytes) -> None:
+        self.id = id
+        self.passphrase = passphrase
+        self.public_key = public_key
+        self.private_key = private_key
+        self.load_succes = True
+
+
+class UnknownRoom(Room):
+    def __init__(self) -> None:
+        super().__init__(None, None, None, None)
+        self.load_succes = False
+
+
+async def create_room(passphrase: str | None = None) -> Room:
+    passphrase = passphrase or generate_passphrase(9, 3, 8)
+    passphrase_sha512 = sha512(passphrase)
+    id = get_room_id(passphrase)
+    keys = generate_rsa_key(passphrase)
+    async with aiosqlite.connect(room_db(id)) as db:
+        sql = await db.cursor()
+        await sql.executescript(
+            """
+            CREATE TABLE IF NOT EXISTS encryption (
+                public_key TEXT NOT NULL,
+                private_key TEXT NOT NULL,
+                passphrase_sha512 TEXT NOT NULL
+            );
+            CREATE TABLE IF NOT EXISTS accounts (
+                nickname TEXT PRIMARY KEY,
+                password_sha256 TEXT NOT NULL
+            );
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nickname TEXT NOT NULL,
+                message TEXT NOT NULL,
+                attachment TEXT,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (nickname) REFERENCES accounts(nickname)
+            )
+            """
+        )
+        await sql.execute(
+            "INSERT INTO encryption (public_key, private_key, passphrase_sha512) VALUES (?, ?, ?)",
+            (keys[1], keys[0], passphrase_sha512)
+            )
+        await db.commit()
+        return Room(id, passphrase, keys[1], keys[0])
+
+
+async def load_room(passphrase: str | None = None) -> Room:
+    id = get_room_id(passphrase)
+    if not os.path.exists(room_db(id)):
+        return UnknownRoom()
+    passphrase_sha512 = sha512(passphrase)
+    async with aiosqlite.connect(room_db(id)) as db:
+        sql = await db.cursor()
+        encryption = await (await sql.execute(
+            "SELECT public_key, private_key, passphrase_sha512 FROM encryption"
+        )).fetchone()
+        if encryption is None:
+            return UnknownRoom()
+        if encryption[2] != passphrase_sha512:
+            return UnknownRoom()
+        public_key, private_key = encryption[0], encryption[1]
+        message = 'Test key 123'
+        try:
+            encrypted = encrypt_message(message, public_key, passphrase)
+            decrypted = decrypt_message(encrypted, private_key, passphrase)
+        except TypeError:
+            return UnknownRoom()
+        if decrypted != message:
+            return UnknownRoom()
+
+        return Room(id, passphrase, public_key, private_key)
+
+
+class User():
+    def __init__(self, nickname: str, password_sha256: str | None = None) -> None:
+        self.nickname = nickname
+        self.password_sha256 = password_sha256
+
+    async def exists(self, db: aiosqlite.Connection) -> bool:
+        sql = await db.cursor()
+        user = await (await sql.execute(
+            "SELECT * FROM accounts WHERE nickname = ?",
+            (self.nickname,)
+        )).fetchone()
+        return (user is not None)
+
+    async def check_password(self, db: aiosqlite.Connection) -> bool:
+        sql = await db.cursor()
+        user = await (await sql.execute(
+            "SELECT * FROM accounts WHERE nickname = ? AND password_sha256 = ?",
+            (self.nickname, self.password_sha256)
+        )).fetchone()
+        return (user is not None)
+
+
+async def create_user(nickname: str, password: str, db: aiosqlite.Connection, sql: aiosqlite.Cursor | None = None) -> User:
+    sql = sql or (await db.cursor())
+    password_sha256 = sha256(password)
+    await sql.execute(
+        "INSERT INTO accounts (nickname, password_sha256) VALUES (?, ?)",
+        (nickname, password_sha256)
+    )
+    return User(nickname, password_sha256)
+
+
+async def test():
+    try:
+        room = await create_room()
+        passphrase = room.passphrase
+        loaded_room = await load_room(passphrase)
+        id = loaded_room.id
+
+        print(f"passphrase: {passphrase}")
+        print(f"room id: {get_room_id(passphrase)}")
+        print(f"room database: {room_db(id)}")
+
+        message = generate_passphrase(5, 1, 5)
+        encrypted = encrypt_message(message, room.public_key, passphrase)
+        decrypted = decrypt_message(encrypted, loaded_room.private_key, passphrase)
+        encryption_test = "Passed" if decrypted == message else "Failed"
+        print(f"\nEncryption test: {encryption_test}")
+
+        async with aiosqlite.connect(room_db(id)) as db:
+            nickname = generate_random_word(7)
+            password = generate_random_word(15)
+            user = await create_user(nickname, password, db)
+            user_exists = await user.exists(db)
+            check_password = await user.check_password(db)
+
+            user2 = user
+            user2.password_sha256 = sha256('a')
+            check_password2 = await user2.check_password(db)
+
+            users_test = "Passed" if user_exists and check_password and not check_password2 else "Failed"
+            print(f"User system test: {users_test}")
+
+        input("\nPress Enter to exit")
+    finally:
+        try:
+            os.remove(room_db(id))
+        except UnboundLocalError:
+            pass
+        except FileNotFoundError:
+            pass
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(test())
---
+++ main.py
@@ -0,0 +1,245 @@
+import asyncio
+import binascii
+from contextlib import asynccontextmanager
+import pywebio
+import pywebio.output as output
+import pywebio.input as input
+import aiosqlite
+import functions
+
+from pywebio.session import run_async
+
+# MAX_MESSAGES_CNT = 200
+
+chat_msgs = []
+
+
+# async def refresh_msg(my_name, msg_box):
+#     global chat_msgs
+#     last_idx = len(chat_msgs)
+#     while True:
+#         await asyncio.sleep(0.5)
+#         for m in chat_msgs[last_idx:]:
+#             if m[0] != my_name:
+#                 msg_box.append(output.put_markdown('`%s`: %s' % m, sanitize=True))
+
+#         # if len(chat_msgs) > MAX_MESSAGES_CNT:
+#         #     del chat_msgs[100:]
+
+#         last_idx = len(chat_msgs)
+
+
+class AsyncDatabaseConnectionManager:
+    _instance = None
+    _lock = asyncio.Lock()
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(AsyncDatabaseConnectionManager, cls).__new__(cls)
+            cls._instance.connections = {}
+        return cls._instance
+
+    async def get_connection(self, db_name):
+        if db_name not in self.connections:
+            self.connections[db_name] = await aiosqlite.connect(db_name)
+        return self.connections[db_name]
+
+    @asynccontextmanager
+    async def connection_context(self, db_name):
+        connection = await self.get_connection(db_name)
+        try:
+            yield connection
+        finally:
+            pass
+
+    async def close_all_connections(self):
+        for connection in self.connections.values():
+            await connection.close()
+        self.connections.clear()
+
+
+db_manager = AsyncDatabaseConnectionManager()
+
+
+async def _input(label: str, *args, **kwargs):
+    name = {"name": "data"}
+    if "name" in kwargs:
+        name = {}
+    _input = await input.input_group(
+            label,
+            inputs=[
+                input.input(
+                    *args, **kwargs, **name
+                ),
+                pywebio.input.actions(
+                    name="action",
+                    buttons=[
+                        {"label": "Submit", "value": "submit", "color": "primary"},
+                        {"label": "Reset", "type": "reset", "color": "warning"}
+                    ]
+                )
+            ])
+    return _input
+
+
+@pywebio.config(theme='dark')
+async def main():
+    output.toast("Created by mrdan__")
+
+    async def join_room():
+        output.close_popup()
+        success = False
+        while not success:
+            passphrase = (await _input("Room key", type=input.PASSWORD, required=True))["data"]
+            room = await functions.load_room(passphrase)
+            success = room.load_succes
+            if not success:
+                output.toast("No room found with this key", duration=3, color="error")
+        success = False
+        username = ""
+        while not success:
+            _username = (await _input("Username", required=True))["data"]
+            agree = (await _input(f"\"{_username}\" is you username? (yes/no)", required=True))["data"]
+            if str(agree).lower() in ["yes", "yh", "ofc", "yeah"]:
+                username = _username
+                success = True
+
+        user = functions.User(username)
+        async with aiosqlite.connect(functions.room_db(room.id)) as db:
+            user_exists = await user.exists(db)
+        if not user_exists:
+            output.toast("This account does not exist, a new one will be created")
+            success = False
+            password = ""
+            while not success:
+                result = await input.input_group(
+                    label="New password",
+                    inputs=[
+                        input.input(type=input.PASSWORD, name="password1", placeholder="Password"),
+                        input.input(type=input.PASSWORD, name="password2", placeholder="Repeat password"),
+                        pywebio.input.actions(
+                            name="action",
+                            buttons=[
+                                {"label": "Submit", "value": "submit", "color": "primary"},
+                                {"label": "Reset", "type": "reset", "color": "warning"}
+                            ]
+                        )
+                    ]
+                )
+                if result["password1"] == result["password2"]:
+                    success = True
+                    password = result["password1"]
+                else:
+                    output.toast("Password mismatch", color="error")
+            async with aiosqlite.connect(functions.room_db(room.id)) as db:
+                user = await functions.create_user(username, password, db)
+                await db.commit()
+        else:
+            success = False
+            while not success:
+                password = (await _input("Password", required=True, type=input.PASSWORD))["data"]
+                user.password_sha256 = functions.sha256(password)
+                async with aiosqlite.connect(functions.room_db(room.id)) as db:
+                    if await user.check_password(db):
+                        success = True
+                    else:
+                        output.toast("Wrong password", color='error')
+        run_async(chat(user, room))
+
+    async def create_room():
+        new_room = await functions.create_room()
+        output.close_popup()
+        output.popup(
+            title='Succes!',
+            content=[
+                output.put_markdown("# If you lose this key you will never be able to enter the room again."),
+                output.put_markdown(f"### Key: \n{new_room.passphrase}"),
+                output.put_button("Join room", onclick=join_room)
+            ],
+            closable=False
+        )
+
+    output.popup(
+        title='Hi! What do you want to do?',
+        content=[
+            output.put_button("Join room", onclick=join_room),
+            output.put_button("Create room", onclick=create_room,)
+        ],
+        closable=False
+    )
+
+
+class RefreshMsg:
+    def __init__(self, msg_box: output.Output, room: functions.Room) -> None:
+        self.msg_box = msg_box
+        self.last_index = 0
+        self.room = room
+        self.messages = {}
+
+    async def refresh_msg(self, db: aiosqlite.Connection):
+        sql = await db.cursor()
+        await asyncio.sleep(1)
+        while True:
+            await asyncio.sleep(0.5)
+            messages = await (await sql.execute(
+                "SELECT id, nickname, message, created_at FROM messages ORDER BY id DESC LIMIT 25"
+            )).fetchall()
+            messages = list(messages)
+            messages.reverse()
+            if self.last_index < messages[len(messages)-1][0]:
+                self.messages = messages
+                for x in messages:
+                    if x[0] <= self.last_index:
+                        continue
+                    try:
+                        msg = functions.decrypt_message(x[2], self.room.private_key, self.room.passphrase)
+                    except binascii.Error:
+                        msg = x[2]
+                    except ValueError:
+                        msg = x[2]
+                    self.msg_box.append(output.put_markdown('`%s`: %s' % (x[1], msg), sanitize=True))
+                if self.last_index == 0:
+                    ...
+                self.last_index = messages[len(messages)-1][0]
+
+
+async def add_msg(
+            msg: str, user: functions.User,
+            room: functions.Room, db: aiosqlite.Connection
+        ):
+    sql = await db.cursor()
+    encrypted_msg = functions.encrypt_message(msg, room.public_key, room.passphrase)
+    await sql.execute(
+        "INSERT INTO messages (nickname, message) VALUES (?, ?)",
+        (user.nickname, encrypted_msg)
+    )
+    await db.commit()
+
+
+async def chat(user: functions.User, room: functions.Room):
+    room_db = functions.room_db(room.id)
+    msg_box = output.output()
+    output.put_scrollable(msg_box, height=300, keep_bottom=True)
+
+    async with db_manager.connection_context(room_db) as db:
+        db: aiosqlite.Connection
+
+        refresher = RefreshMsg(msg_box, room)
+        run_async(refresher.refresh_msg(db))
+
+        while True:
+            data = await input.input_group('Send message', [
+                input.input(name='msg', help_text='Message content supports inline Markdown syntax'),
+                input.actions(name='cmd', buttons=['Send', 'Multiline Input', {'label': 'Exit', 'type': 'cancel'}])
+            ], validate=lambda d: ('msg', 'Message content cannot be empty') if d['cmd'] == 'Send' and not d['msg'] else None)
+            if data is None:
+                break
+            if data['cmd'] == 'Multiline Input':
+                data['msg'] = '\n' + await input.textarea('Message content', help_text='Message content supports Markdown syntax')
+            await add_msg(data['msg'], user, room, db)
+
+    output.toast("You have left the chat room")
+
+
+if __name__ == '__main__':
+    pywebio.start_server(main, debug=True, port=9999)
